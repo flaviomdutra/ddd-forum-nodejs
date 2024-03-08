7cac00e7342bca6034b26e56861833ec9465af15
Flávio Dutra <flaviomdutra@outlook.com>
2024-03-08 20:58:47 -0300

    Add save method to QuestionsRepository

---
+++ src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -0,0 +1,57 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { makeQuestion } from 'test/factories/make-question'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { EditQuestionUseCase } from './edit-question'
+
+describe('Edit Question', () => {
+  let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+  let sut: EditQuestionUseCase
+
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new EditQuestionUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to edit a question', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityID('author-id'),
+      },
+      new UniqueEntityID('question-id'),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    await sut.execute({
+      authorId: 'author-id',
+      questionId: 'question-id',
+      title: 'new-title',
+      content: 'new-content',
+    })
+
+    expect(inMemoryQuestionsRepository.items[0]).toMatchObject({
+      title: 'new-title',
+      content: 'new-content',
+    })
+  })
+
+  it('should not be able to edit a question from another user', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityID('author-id'),
+      },
+      new UniqueEntityID('question-id'),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    await expect(
+      sut.execute({
+        authorId: 'author-wrong-id',
+        questionId: 'question-id',
+        title: 'new-title',
+        content: 'new-content',
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+})
