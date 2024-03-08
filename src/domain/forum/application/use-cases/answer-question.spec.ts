6798ebdc796e6fd9fe2f2e85c7766d0573c596a6
Flávio Dutra <flaviomdutra@outlook.com>
2024-03-08 20:01:52 -0300

    Refactor use case tests and use in-memory repositories

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,18 +1,24 @@
-import { AnswersRepository } from '../repositories/answers-repository'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
 import { AnswerQuestionUseCase } from './answer-question'
 
-const fakeAnswersRepository: AnswersRepository = {
-  create: async () => {},
-}
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: AnswerQuestionUseCase
 
-test('create an answer', async () => {
-  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
-
-  const answer = await answerQuestion.execute({
-    instructorId: 'any_id',
-    questionId: 'any_id',
-    content: 'any_content',
+describe('Answer Question', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new AnswerQuestionUseCase(inMemoryAnswersRepository)
   })
 
-  expect(answer.content).toEqual('any_content')
+  it('should be able to create an answer', async () => {
+    const { answer } = await sut.execute({
+      instructorId: 'any_id',
+      questionId: 'any_id',
+      content: 'any_content',
+    })
+
+    expect(answer.id).toBeTruthy()
+    expect(answer.content).toEqual('any_content')
+    expect(inMemoryAnswersRepository.items[0]).toEqual(answer)
+  })
 })
