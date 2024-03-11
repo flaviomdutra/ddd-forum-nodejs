90048c8fc1ea91b26321620408384a5a8abe9a80
Flávio Dutra <flaviomdutra@outlook.com>
2024-03-11 10:45:45 -0300

    Refactor question attachments to use QuestionAttachmentList

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -3,12 +3,12 @@ import { Optional } from '@/core/types/optional'
 import dayjs from 'dayjs'
 import { Slug } from './value-objects/slug'
 import { AggregateRoot } from '@/core/entities/aggregate-root'
-import { QuestionAttachment } from './question-attachment'
+import { QuestionAttachmentList } from './question-attachment-list'
 
 export interface QuestionProps {
   authorId: UniqueEntityID
   bestAnswerId?: UniqueEntityID
-  attachments: QuestionAttachment[]
+  attachments: QuestionAttachmentList
   title: string
   content: string
   slug: Slug
@@ -25,7 +25,7 @@ export class Question extends AggregateRoot<QuestionProps> {
       {
         ...props,
         slug: props.slug ?? Slug.createFromText(props.title),
-        attachments: props.attachments ?? [],
+        attachments: props.attachments ?? new QuestionAttachmentList(),
         createdAt: props.createdAt ?? new Date(),
       },
       id,
@@ -70,7 +70,7 @@ export class Question extends AggregateRoot<QuestionProps> {
     return this.content.substring(0, 120).trimEnd().concat('...')
   }
 
-  get attachments(): QuestionAttachment[] {
+  get attachments(): QuestionAttachmentList {
     return this.props.attachments
   }
 
@@ -94,7 +94,7 @@ export class Question extends AggregateRoot<QuestionProps> {
     this.touch()
   }
 
-  set attachments(attachments: QuestionAttachment[]) {
+  set attachments(attachments: QuestionAttachmentList) {
     this.props.attachments = attachments
   }
 }
